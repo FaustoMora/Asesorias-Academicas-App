4764d0de0c43520ceea32328a3df42edb81035fa
omnilasher <raywarcraft@hotmail.com>
2017-01-30 19:50:10 -0500

    Agregando archivo de prueba

--- AsAcApp/resources/views/hola_mundo.blade.php
+++ AsAcApp/resources/views/hola_mundo.blade.php
@@ -39,7 +39,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Hola Mundo </div>
+				<div class="title">Hola Mundo</div>
 				<div class="quote">Inicio del proyecto</div>
 				<div class="quote">
 				<?php
