6346b4db70e562fd1eee9270f02e665a60745880
omnilasher <raywarcraft@hotmail.com>
2017-04-09 15:13:38 -0500

    merge2

--- AsAcApp/database/migrations/2017_02_05_020600_create_materia_table.php
+++ AsAcApp/database/migrations/2017_02_05_020600_create_materia_table.php
@@ -16,13 +16,8 @@ class CreateMateriaTable extends Migration
         Schema::create('materia', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nombre_materia');
-<<<<<<< HEAD:AsAcApp/database/migrations/2017_04_07_031126_create_materia_table.php
-            $table->string('icono_materia')->nullable;
-            $table->integer('user_id')->unsigned();
-=======
             $table->mediumText('icono_materia')->nullable();
             $table->integer('user_id')->unsigned()->nullable();
->>>>>>> 2caeb74856732df604ebc4c2b87049633a79c563:AsAcApp/database/migrations/2017_02_05_020600_create_materia_table.php
             $table->timestamps();
         });
          Schema::table('materia', function(Blueprint $table){
